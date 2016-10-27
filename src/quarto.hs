e22ee4dcfb2a16920c013900a17ea891ce2515b3
Matt Breckon <mattbreckon@gmail.com>
2016-10-27 15:41:44 -0400

    Learning haskell

--- src/quarto.hs
+++ src/quarto.hs
@@ -1,19 +1,52 @@
 module Quarto where
 
-data Piece = Piece { height :: String,
-                    top :: String,
-                    colour :: String,
-                    shape :: String
+import Data.List
+import Data.Maybe
+
+-- Board
+type Board = [[Tile]]
+type Tile = Maybe Piece
+data Player = P1 | P2 deriving (Eq, Show)
+data Game = Game { board :: Board, playerTurn :: Player }
+
+-- Piece
+type Characteristic = String
+-- data Position = Position (Int, Int) deriving (Eq, Show)
+data Piece = Piece { height :: Characteristic,
+                    top :: Characteristic,
+                    colour :: Characteristic,
+                    shape :: Characteristic
                     } deriving (Eq, Show)
 
-pieceHeight :: Piece -> String
+initBoard = [[Nothing,Nothing,Nothing,Nothing]
+            ,[Nothing,Nothing,Nothing,Nothing]
+            ,[Nothing,Nothing,Nothing,Nothing]
+            ,[Nothing,Nothing,Nothing,Nothing]
+            ]
+
+piece :: String -> String -> String -> String -> Piece
+piece h t c s = Piece {height = h, top = t, colour = c, shape = s}
+
+-- 2D print board
+printTile tile =  putStr $ (show tile) ++ "\t"
+printBoard board
+  | length (nub [length row | row <- board]) /= 1 = error "2D list not symmetric"
+  | otherwise = mapM_ printRow board
+    where printRow row = (mapM_ printTile) row >> putStrLn ""
+
+pieceHeight :: Piece -> Characteristic
 pieceHeight (Piece {height = h, top = t, colour = c, shape = s}) = h
 
-pieceTop :: Piece -> String
+pieceTop :: Piece -> Characteristic
 pieceTop (Piece {height = h, top = t, colour = c, shape = s}) = t
 
-pieceColour :: Piece -> String
+pieceColour :: Piece -> Characteristic
 pieceColour (Piece {height = h, top = t, colour = c, shape = s}) = c
 
-pieceShape :: Piece -> String
+pieceShape :: Piece -> Characteristic
 pieceShape (Piece {height = h, top = t, colour = c, shape = s}) = s
+
+main = do
+  let x = piece "tall" "hollow" "red" "circle"
+  putStrLn $ show (pieceHeight x)
+  -- putStrLn $ show initBoard
